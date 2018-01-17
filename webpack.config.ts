fef0cfe1fc5f7c3ef70875caec69cab2b9629c8f
Cecile Muller <git@wildpeaks.fr>
2018-01-17 16:05:44 +0100

    Better types in Webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,14 @@
 /* eslint-env node */
 import {join} from 'path';
-import {DefinePlugin, LoaderOptionsPlugin} from 'webpack';
+import {DefinePlugin, LoaderOptionsPlugin, Configuration, Plugin, Rule} from 'webpack';
 import {CheckerPlugin} from 'awesome-typescript-loader';
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const config = {
-	target: 'node',
-	devtool: 'source-map',
-	entry: './src/script.ts',
-	output: {
-		filename: 'script.min.js',
-		path: join(__dirname, 'lib'),
-		publicPath: '/',
-		crossOriginLoading: 'anonymous'
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	plugins: [
+
+export default function(): Configuration {
+	//region Plugins
+	const plugins: Plugin[] = [
 		new CheckerPlugin(),
 		new DefinePlugin({
 			'process.env.NODE_ENV': '"production"'
@@ -36,35 +26,54 @@ const config = {
 			root: __dirname,
 			verbose: false
 		})
-	],
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.js$/,
-				use: 'source-map-loader'
-			},
-			{
-				enforce: 'pre',
-				test: /\.ts?$/,
-				use: 'source-map-loader'
-			},
-			{
-				test: /\.ts$/,
-				use: [
-					{
-						loader: 'awesome-typescript-loader',
-						options: {
-							transpileOnly: true
-						}
-					}
-				]
-			}
-		]
-	},
-	performance: {
-		hints: false
-	}
-};
+	];
+	//endregion
 
-export default config;
+	//region Loaders
+	const rules: Rule[] = [
+		{
+			enforce: 'pre',
+			test: /\.js$/,
+			use: 'source-map-loader'
+		},
+		{
+			enforce: 'pre',
+			test: /\.ts?$/,
+			use: 'source-map-loader'
+		},
+		{
+			test: /\.ts$/,
+			use: [
+				{
+					loader: 'awesome-typescript-loader',
+					options: {
+						transpileOnly: true
+					}
+				}
+			]
+		}
+	];
+	//endregion
+
+	return {
+		target: 'node',
+		devtool: 'source-map',
+		entry: './src/script.ts',
+		output: {
+			filename: 'script.min.js',
+			path: join(__dirname, 'lib'),
+			publicPath: '/',
+			crossOriginLoading: 'anonymous'
+		},
+		resolve: {
+			extensions: ['.ts', '.js']
+		},
+		plugins,
+		module: {
+			rules
+		},
+		performance: {
+			hints: false
+		}
+	};
+}
